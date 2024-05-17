ad2d304fbbdf633bc6918becbf14d6290219029d
pi <pi@MyVoron-5metppe>
2024-05-17 12:56:02 +0200

    New backup from 17/05/24 - 12:56:02

--- printer_data/scripts/csv2png.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-echo  "************ Analyzing Inputshaper data for X axis ************"
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x.png
-echo 
-echo
-echo  "************ Analyzing Inputshaper data for Y axis ************"
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png
-
